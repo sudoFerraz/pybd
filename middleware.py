742f1b275e45ec04c29b243c66c1e069b06eb42a
root <ferraz@ufu.br>
2016-11-10 02:23:20 +0000

    Started the middleware helper

--- middleware.py
+++ middleware.py
@@ -1,1 +1,33 @@
-import structures.py
+import structures
+
+main = structures.Directory()
+
+def movereg():
+    """Move a rid from a page to another."""
+
+def createrelation(name, directory):
+    """Create a new relation on the system."""
+    if len(main.ridindex) == 0 and len(main.pagesindex) == 0:
+        newrid = structures.Rid(name, main)
+    else:
+        newrid = structures.Rid(name, main)
+
+
+def createreg(relation):
+    """Create a new tuple of a relation."""
+    newreg = structures.Reg(relation)
+
+def delrelation():
+    """Delete a relation present on the system."""
+
+def delreg():
+    """Delete a tuple of a relation."""
+
+def searchrelation():
+    """Search for a given relation present in the system."""
+
+def searchreg():
+    """Search for a tuple of a relation present in the system."""
+
+def returnpage():
+    """Return a page that cointains certain rid."""
--- structures.py
+++ structures.py
@@ -16,7 +16,7 @@ class Page():
         if len(self.regindex) <= size:
             self.regindex.append(reg)
             self.nreg = self.nreg + 1
-            self.directory.ridindex[reg.uid] = self.uid
+            self.directory.ridindex[reg.rid.uid] = self.uid
         else:
             newpage = Page()
             self.nextpage = newpage
@@ -24,16 +24,16 @@ class Page():
             newpage.nreg = 1
             newpage.regindex.append(reg)
             self.directory.pagesindex.append(newpage)
-            self.directory.ridindex[reg.uid] = newpage.uid
+            self.directory.ridindex[reg.rid.uid] = newpage.uid
 
     def delreg(self, reg):
         if self.nreg == 1 and len(self.regindex) == 1:
-            del self.directory.ridindex[reg.uid]
+            del self.directory.ridindex[reg.rid.uid]
             del self
         else:
-            del self.regindex[reg.uid]
+            del self.regindex[reg.rid.uid]
             self.nreg = self.nreg - 1
-            del self.directory.ridindex[reg.uid]
+            del self.directory.ridindex[reg.rid.uid]
 
 
 class Data():
@@ -51,14 +51,17 @@ class Directory():
         self.hash = str(uuid.uuid4().hex)
         self.pagesindex = []
         self.ridindex = {}
+        self.ridlist = []
 
 class Rid():
     """Relations present in the database."""
 
-    def __init__(self, name):
+    def __init__(self, name, directory):
         self.uid = str(uuid.uuid4().hex)
         self.regindex = []
         self.name = name
+        self.directory = directory
+        self.directory.ridlist.append(self.uid)
 
 
 class Reg():
