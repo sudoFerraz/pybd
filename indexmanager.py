c47136b0c16f35e5ffa36f4fee16fa3f0552dedd
root <ferraz@ufu.br>
2016-11-11 04:48:32 +0000

    Index manager defined and starting to code the user aplication that acts on bufferpool

---
+++ indexmanager.py
@@ -0,0 +1,26 @@
+import structures
+import middleware
+import bufferpool
+import subpolitics
+
+def crescentindex():
+    """Put the objects inside of an index in a crescent order."""
+
+def btreeindex():
+    """Organize the index in form of a b+ tree."""
+
+def searchtree():
+    """Searching a tree of index to retrieve a tuple."""
+
+def binarysearch():
+    """Searching for a frame that contains a certain relation."""
+
+def changekey():
+    """Changing a key of a tuple inside a frame."""
+
+def removetuple():
+    """Remove a tuple from a frame."""
+
+def addtuple():
+    """Adding a tuple to a frame."""
+
